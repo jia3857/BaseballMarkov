478dcbc285636e4777fba011aa21c642db690ba0
Josh Yeh <jjyeh@cloudera.com>
2018-05-01 18:45:23 +0000

    Added https://gist.github.com/bayesball/8892981

--- Examples.R
+++ Examples.R
@@ -6,23 +6,39 @@ source('GameSimulation.R')
 event= "..."
 csv = ".../data"
 
-LAA_2014=create_lineup(player_names=c('Kole Calhoun','Mike Trout', 'Albert Pujols','Josh Hamilton',
-                                    'Howie Kendrick','Erick Aybar','David Freese', 'Chris Iannetta', 'Collin Cowgill'), 
-                       player_seasons=rep(2014,9), 
-                       event_file_dir=event, 
-                       csv_dir=csv)
+## LAA_2014
+#LAA_2014 = create_lineup(
+#  player_names=c(
+#    'Kole Calhoun',
+#    'Mike Trout', 
+#    'Albert Pujols',
+#    'Josh Hamilton',
+#    'Howie Kendrick',
+#    'Erick Aybar',
+#    'David Freese', 
+#    'Chris Iannetta', 
+#    'Collin Cowgill'), 
+#  player_seasons=rep(2015,9),
+#  event_file_dir=event,
+#  csv_dir=csv)
+#season = simulate_season(LAA_2014,'StateRuns.csv')
+#cat("Averaged Runs of each game: ", sum(season)/162)
 
-BARRY_MVP=create_lineup(player_names=rep('Barry Bonds',9), 
-                     player_seasons=rep(2001,9), 
-                     event_file_dir=event, 
-                     csv_dir=csv)
+## BARRY_MVP
+#BARRY_MVP=create_lineup(player_names=rep('Barry Bonds',9), 
+#                     player_seasons=rep(2001,9), 
+#                     event_file_dir=event, 
+#                     csv_dir=csv)
+#season2 = simulate_season(BARRY_MVP,'StateRuns.csv')
+#cat("Averaged Runs of each game: ", sum(season2)/162)
 
-MLB_AVERAGE_2009=create_lineup(player_names=rep(' ',9), 
-                     player_seasons=rep(2009,9), 
-                     event_file_dir=event, 
-                     csv_dir=csv)
+## MLB_AVERAGE_2009
+#MLB_AVERAGE_2009=create_lineup(player_names=rep(' ',9), 
+#                     player_seasons=rep(2009,9), 
+#                     event_file_dir=event, 
+#                     csv_dir=csv)
+#season3 = simulate_season(MLB_AVERAGE_2009,'StateRuns.csv')
+#cat("Averaged Runs of each game: ", sum(season3)/162)
 
 
-season = simulate_season(LAA_2014,'StateRuns.csv')
-season2 = simulate_season(BARRY_MVP,'StateRuns.csv')
-season3 = simulate_season(MLB_AVERAGE_2009,'StateRuns.csv')
+
